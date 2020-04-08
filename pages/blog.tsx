ef9a202787bd73d28b1f1fea4231180fe7a74dba
@kickbutovski34 <kickbutovski34@gmail.com>
2020-04-08 15:16:42 +0300

    create empty project and add main pages

---
+++ pages/blog.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from "next";
+import React from "react";
+
+const Blog: NextPage = () => <h1>Blog Page</h1>;
+
+export default Blog;
---
+++ pages/services.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from "next";
+import React from "react";
+
+const Services: NextPage = () => <h1>Services Page</h1>;
+
+export default Services;
