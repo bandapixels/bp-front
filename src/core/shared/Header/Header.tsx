b8b2d059352d5df9f955239c9f05e4838bdf49e4
@kickbutovski34 <kickbutovski34@gmail.com>
2020-04-15 12:29:16 +0300

    remove component HeaderView

--- src/core/shared/Header/Header.tsx
+++ src/core/shared/Header/Header.tsx
@@ -1,8 +1,35 @@
-import React from "react";
-import HeaderView from "./HeaderView";
+import React, { useState } from "react";
+import classNames from "classnames";
+import Button from "../coreUi/Button/Button";
+import MenuList from "./components/MenuList/MenuList";
+import OpenedMobileMenu from "./components/OpenedMobileMenu/OpenedMobileMenu";
 
-const Header: React.FunctionComponent = () => {
-  return <HeaderView />;
+import styles from "./header.module.scss";
+
+const HeaderView: React.FunctionComponent = () => {
+  const [openedMenu, setOpenedMenu] = useState(false);
+
+  const handlerOpenMobileMenu = (): void => {
+    setOpenedMenu(!openedMenu);
+  };
+
+  const headerStyles = classNames(styles.header, {
+    openedMenuWrapper: openedMenu
+  });
+
+  return (
+    <header className={headerStyles}>
+      <div className={styles.logoWrapper}>
+        <div className={styles.bigBlackLogo} />
+      </div>
+      <div className={styles.headerLeftPart}>
+        <MenuList />
+        <Button classes="headerBtn" title="discuss the project" />
+        <Button classes="menuSwitchBtn" handlerClick={handlerOpenMobileMenu} />
+      </div>
+      {openedMenu && <OpenedMobileMenu />}
+    </header>
+  );
 };
 
-export default Header;
+export default HeaderView;
