fb195f68f57ccb1b76ec3add9af531fc652f136d
@kickbutovski34 <kickbutovski34@gmail.com>
2020-04-21 14:59:03 +0300

    stylized contacts block

--- src/core/shared/ContactsBlock/ContactsBlock.tsx
+++ src/core/shared/ContactsBlock/ContactsBlock.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { createGrid, addAnimationToGrid } from "../../utils/grid";
+
+import SocialList from "../Header/components/SocialList/SocialList";
+
 import styles from "./contactsBlock.module.scss";
 
 const ConstactsBlock: React.FunctionComponent = () => {
+  const refGridWrapper = useRef<HTMLDivElement>();
+
+  useEffect(() => {
+    const mainWrapper = refGridWrapper.current;
+
+    createGrid(mainWrapper, 75);
+
+    document.addEventListener("mousemove", e => {
+      addAnimationToGrid(e, "rgba(23,23,24,.1)", "#171718", mainWrapper);
+    });
+  }, []);
+
   return (
-    <section className={styles.contactsWrapper}>
+    <section className={styles.contactsWrapper} ref={refGridWrapper}>
       <div className={styles.contactsContent}>
         <h3 className={styles.contactsTitle}>
-          Lets make your service the best choice for the users
+          Lets make your service
+          <br /> the best choice for the users
         </h3>
         <p className={styles.contactsText}>
-          our team the best choice for you
+          Our team the best choice for you
           <br />
           Join vs work with us
         </p>
@@ -17,7 +34,12 @@ const ConstactsBlock: React.FunctionComponent = () => {
           team@bandapixels.com
         </a>
       </div>
-      <div className={styles.contactsSocial}>Social</div>
+      <div className={styles.contactsSocial}>
+        <SocialList />
+      </div>
+      <a href="mailto:team@bandapixels.com" className={styles.mailBtnMob}>
+        team@bandapixels.com
+      </a>
     </section>
   );
 };
