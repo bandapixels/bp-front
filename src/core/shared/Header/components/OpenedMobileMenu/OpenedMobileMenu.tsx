8f54ad747f7d735d2d5d0b02fcda40ccb50be9a9
@kickbutovski34 <kickbutovski34@gmail.com>
2020-04-22 15:11:59 +0300

    added animation when menu closing

--- src/core/shared/Header/components/OpenedMobileMenu/OpenedMobileMenu.tsx
+++ src/core/shared/Header/components/OpenedMobileMenu/OpenedMobileMenu.tsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import classNames from "classnames";
 import MenuList from "../MenuList/MenuList";
 import SocialList from "../SocialList/SocialList";
 import styles from "./openedMobileMenu.module.scss";
 
-const OpenedMobileMenu: React.FunctionComponent = () => {
+interface MenuProps {
+  openedMenu?: boolean;
+}
+
+const OpenedMobileMenu: React.FunctionComponent<MenuProps> = ({
+  openedMenu
+}) => {
+  const [shouldRender, setRender] = useState(openedMenu);
+  const classes = classNames(styles.openedMobileWrapper, {
+    slideUp: !openedMenu,
+    slideDown: openedMenu
+  });
+
+  useEffect(() => {
+    if (openedMenu) setRender(true);
+  }, [openedMenu]);
+
+  const onAnimationEnd = (): void => {
+    if (!openedMenu) setRender(false);
+  };
+
   return (
-    <div className={styles.openedMobileWrapper}>
-      <MenuList />
-      <div>
-        <SocialList />
-        <a href="mailto:team@bandapixels.com" className={styles.mailBtn}>
-          team@bandapixels.com
-        </a>
+    shouldRender && (
+      <div className={classes} onAnimationEnd={onAnimationEnd}>
+        <MenuList />
+        <div>
+          <SocialList />
+          <a href="mailto:team@bandapixels.com" className={styles.mailBtn}>
+            team@bandapixels.com
+          </a>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
