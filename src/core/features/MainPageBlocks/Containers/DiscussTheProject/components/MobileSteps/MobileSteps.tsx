8ffe5988a2506b95bfe69a0ecbf1cf6ba21f41df
@kickbutovski34 <kickbutovski34@gmail.com>
2020-04-23 10:47:55 +0300

    added font open sans for radio buttons

--- src/core/features/MainPageBlocks/Containers/DiscussTheProject/components/MobileSteps/MobileSteps.tsx
+++ src/core/features/MainPageBlocks/Containers/DiscussTheProject/components/MobileSteps/MobileSteps.tsx
@@ -6,9 +6,9 @@ import Button from "../../../../../../shared/coreUi/Button/Button";
 const MobileSteps: React.FunctionComponent = () => {
   return (
     <div className={styles.stepWrapper}>
-      <p className={styles.currentStep}>1/2</p>
+      <p className={styles.currentStep}>1 / 2</p>
       <div className={styles.stepProgress} />
-      <Button classes="yellowBtnWithIcon">
+      <Button classes="yellowBtnWithIcon" type="button">
         Next Step
         <img src="/images/icons/arrow.svg" alt="arrow" />
       </Button>
