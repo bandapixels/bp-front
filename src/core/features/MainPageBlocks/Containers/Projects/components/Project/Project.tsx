c617354aaeb741168cdfd424e384011f9dfa10a9
@kickbutovski34 <kickbutovski34@gmail.com>
2020-05-04 17:07:46 +0300

    show more

--- src/core/features/MainPageBlocks/Containers/Projects/components/Project/Project.tsx
+++ src/core/features/MainPageBlocks/Containers/Projects/components/Project/Project.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import styles from "./project.module.scss";
 
 interface ProjectProps {
@@ -16,13 +17,43 @@ const Project: React.FunctionComponent<ProjectProps> = ({
   video,
   url
 }) => {
+  const [visiblePart, setVisiblePart] = useState(problem);
+  const [hiddenPart, setHiddenPart] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  const splitTextForMobile = (): void => {
+    if (globalThis.outerWidth <= 668) {
+      const textArr = visiblePart.split("").filter(word => word.length > 0);
+
+      if (textArr.length > 120) {
+        setVisiblePart(textArr.slice(0, 120).join(""));
+        setHiddenPart(textArr.slice(120).join(""));
+      }
+    }
+  };
+
+  const handlerShowMore = (): void => {
+    setVisible(!visible);
+  };
+
+  useEffect(() => {
+    splitTextForMobile();
+  }, []);
+
   return (
     <div className={styles.projectItem}>
       <div className={styles.projectContent}>
         <div className={styles.projectInfo}>
           <h3 className={styles.projectTitle}>What problem do we solve</h3>
-          <div className={styles.projectProblem}>{problem}</div>
-          <span className={styles.readMore}>read more</span>
+          <div className={styles.projectProblem}>
+            {visiblePart}
+            {hiddenPart && visible ? { hiddenPart } : "..."}
+          </div>
+          {!visible && (
+            <span className={styles.readMore} onClick={handlerShowMore}>
+              read more
+            </span>
+          )}
         </div>
         <div className={styles.projectMedia} data-name={video} />
       </div>
