7a95f57c80ccbd18409ed0432aa3bb38de4d40fa
naydenov_igor <etcetc1820@gmail.com>
2020-05-29 11:22:53 +0300

    added noindex to the page projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { NextPage } from "next";
+import Head from "next/head";
 import StoreLayout from "../src/core/layouts/StoreLayout";
 import Projects from "../src/core/features/MainPageBlocks/Containers/Projects/Projects";
 
 const Contacts: NextPage = () => {
   return (
-    <StoreLayout>
-      <Projects />
-    </StoreLayout>
+    <>
+      <Head>
+        <meta name="robots" content="noindex" />
+        <meta name="googlebot" content="noindex" />
+      </Head>
+      <StoreLayout>
+        <Projects />
+      </StoreLayout>
+    </>
   );
 };
 
