6c3ce1749611bf140cef0be509fc2dd4db8fdf53
etcetc1820 <etcetc1820@gmail.com>
2020-12-03 14:37:40 +0200

    opg, schema

--- pages/services.tsx
+++ pages/services.tsx
@@ -12,6 +12,42 @@ const Services: NextPage = () => {
     <StoreLayout>
       <Head>
         <title>Services</title>
+        <meta property="og:title" content="Bandapixels" />
+        <meta
+          property="og:description"
+          content="Lets make your service the best choice for the users"
+        />
+        <meta
+          property="og:image"
+          content="https://banda-copy.vercel.app/images/banda-logo.jpg"
+        />
+        <meta property="og:image:type" content="image/jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Bandapixels logo" />
+        <meta
+          property="og:url"
+          content="https://banda-copy.vercel.app/services"
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `{
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "url": "https://banda-copy.vercel.app",
+            "email": "team(at)bandapixels.com",
+            "logo": "https://banda-copy.vercel.app/images/banda-logo.jpg",
+            "legalName": "Bandapixels",
+            "sameAs" : [
+              "https://www.behance.net/bandapixels",
+              "https://www.facebook.com/BandaPixels",
+              "https://dribbble.com/bandapixels",
+              "https://www.linkedin.com/organization-guest/company/bandapixels"
+            ]
+          }`
+          }}
+        />
       </Head>
       <FullPageScroll>
         <WeOffer />
